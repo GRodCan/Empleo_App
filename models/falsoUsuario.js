ca3e7e771f9b2af351c81f5819336a8e56c13aa7
Alvaro <alvaroariasrivero92@gmail.com>
2021-11-24 17:24:59 +0100

    creado usuariofalso y vista usuarios, corregido menu

---
+++ models/falsoUsuario.js
@@ -0,0 +1,20 @@
+const users = [
+    {"user": 1,
+    "name": "userName",
+    "email": "email",
+    "password": "password",
+    "admin": "admin",
+    "date": "date",
+    "image": "avatar"
+    }, 
+    {"user": 1,
+    "name": "userName",
+    "email": "email",
+    "password": "password",
+    "admin": "admin",
+    "date": "date",
+    "image": "avatar"
+    }
+]
+
+module.exports = users
