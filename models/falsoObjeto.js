17527b767b57688c7a89e33f4fa4bcd7ca393291
AliRodGon <aliciarodri.ar@gmail.com>
2021-11-24 15:47:13 +0100

    dashboard y favorites arreglados

--- models/falsoObjeto.js
+++ models/falsoObjeto.js
@@ -1,17 +1,16 @@
 
-
 const offerts= [
-    {offert: 1,
-    title: offertTitle,
-    salary: offertSalary,
-    company: company,
-    description: description
+    {"offert": 1,
+    "title": "offertTitle",
+    "salary": "offertSalary",
+    "company": "company",
+    "description": "description"
 }, 
-    {offert: 2,
-    title: offertTitle,
-    salary: offertSalary,
-    company: company,
-    description: description}
+    {"offert": 2,
+    "title": "offertTitle",
+    "salary": "offertSalary",
+    "company": "company",
+    "description": "description"}
 ]
 
 module.exports=offerts
