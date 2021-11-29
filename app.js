c21d862ea2a1eba4d332a6e8ac2dd2c3e9f65725
GRodCan <g.rodcan@outlook.es>
2021-11-29 17:09:21 +0100

    DOM of dashboard

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const apiSqlRouter=require('./routes/api');
 
 const app = express()
 const port = 3000
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.use("/", viewsRouter)
--- controllers/api_mongo.js
+++ controllers/api_mongo.js
@@ -6,9 +6,6 @@ const offerts={
             const query={};
             const cursor = await collection.find(query);
             const data= await cursor.toArray();
-            console.log("*******************************************************************************")
-            console.log(data)
-            console.log("*******************************************************************************")
             res.status(200).render('home', {data:data})
             }catch(err){
                 res.status(400).json({"error":err})
@@ -17,10 +14,10 @@ const offerts={
 
     createOffert: async (req,res)=>{
         try{
-            const offert = new Offert(req.body); //genera nuevo documento con la info recibida del req
+            const offert = await new Offert(req.body); //genera nuevo documento con la info recibida del req
             await collection.insertOne(offert); //lo guarda en BBDD
-            res.status(201).json(offert);
             console.log("Offert created!");
+            res.status(201).redirect("/dashboard");
             }catch(err){
                 res.status(400).json({"error":err})
         }
