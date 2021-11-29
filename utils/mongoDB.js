5b460d74e39cf9509032a44c62077bdc03705f37
Alicia Rod <aliciarodri.ar@gmail.com>
2021-11-29 18:24:03 +0100

    css basico

--- utils/mongoDB.js
+++ utils/mongoDB.js
@@ -1,13 +1,13 @@
-const { MongoClient } = require('mongodb');
+// const { MongoClient } = require('mongodb');
 
-const uri = process.env.DATABASE_MONGO;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-    if(err){throw err}
-    console.log("Connect MongoDB sucess");
-})
-const db = client.db("EmpleoApp")
-const col= db.collection("Offerts")
+// const uri = process.env.DATABASE_MONGO;
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//     if(err){throw err}
+//     console.log("Connect MongoDB sucess");
+// })
+// const db = client.db("EmpleoApp")
+// const col= db.collection("Offerts")
 
 
-module.exports=col
+// module.exports=col
