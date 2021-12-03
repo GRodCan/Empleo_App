189d48f0ccee7b5930284abc63cb27b0b52ad37b
GRodCan <g.rodcan@outlook.es>
2021-12-03 13:56:39 +0100

    users.pug print sql rows

--- controllers/views.js
+++ controllers/views.js
@@ -2,6 +2,7 @@ const falsoObjeto=require ("../models/falsoObjeto.js")
 const falsoUsuario = require ('../models/falsoUsuario')
 const apiMongo= require('../utils/mongoDB')
 const offertSQL = require('../models/favorites')
+const usersSQL = require('../models/user')
 
 const views ={
     home: (req, res) => {        
@@ -20,8 +21,9 @@ const views ={
     profile: (req, res) => {
         res.status(200).render('profile', {users: falsoUsuario})
       },
-    users: (req, res) => {
-        res.status(200).render('users', {users: falsoUsuario})
+    users: async (req, res) => {
+      const users = await usersSQL.getUserByEmail()
+        res.status(200).render('users', {users: users})
       },
     dashboard: async (req, res) => {
         const offerts= await apiMongo.getOfferts()
