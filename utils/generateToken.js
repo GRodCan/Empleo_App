673599bf8e8587a68e9052106bf65dc2b99984e3
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2021-12-10 09:44:35 +0100

    corregidos estilos y eliminados console.log

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -15,9 +15,7 @@ const generateToken=async(req,res)=>{
         email: user.email,
         pass: user.pass
     }
-    console.log("esto es payload", payload)
     const token = jwt.sign(payload, secret, {expiresIn: '1d'})
-    console.log("Esto es token", token)
     res
     .cookie('jwt',
         token,{
